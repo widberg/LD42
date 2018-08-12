97b9ca4d7ff289262446ddb9d06657f455183a5e
ProWolf <6676723+prowolf@users.noreply.github.com>
2018-08-11 20:00:00 -0400

    UI work and bug fixes

--- scripts/Debris.gd
+++ scripts/Debris.gd
@@ -1,13 +1,12 @@
 extends Node
 
 var spawn_time = 1.0
-var objectNames = ["Tire", "Trash", "Mug"]
+var objectNames = ["Tire", "Trash", "Mug", "Pot", "Brick"]
 var objects = []
 var should_spawn
 var timer
 var new_transform
 var debris
-var rot_speed
 
 func _ready():
 	for objectName in objectNames:
@@ -29,9 +28,6 @@ func _physics_process(delta):
 		new_transform.origin = Vector2(rand_range(64, 1856), rand_range(64, 1016))
 		debris.set_transform(new_transform)
 		debris.apply_impulse(Vector2(), Vector2(rand_range(-250, 250), rand_range(-250, 250)))
-		rot_speed = rand_range(-15, 15)
-		debris.apply_impulse(Vector2(0, -32), Vector2(rot_speed, 0));
-		debris.apply_impulse(Vector2(0, 32), Vector2(-rot_speed, 0));
 		get_node("/root/Background/Debris").add_child(debris)
 		should_spawn = false
 	pass
